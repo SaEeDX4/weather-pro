007972d59bf26e9ac44c393e63748b9ff0ea0cb6
SaEeDXIII <saeed.sharifzadeh91@gmail.com>
2025-09-09 16:46:28 -0700

    Fix deploy scripts and base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/weather-pro/", // ← MUST match your repo name
   plugins: [react()],
 });
